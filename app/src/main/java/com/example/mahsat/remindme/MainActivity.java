aa7114d6dfe7f01e2e99f15ea436f82e595924bc
mahsat <fahrenheitx@list.ru>
2016-05-16 16:59:46 +0300

    Toolbar

---
+++ app/src/main/java/com/example/mahsat/remindme/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.mahsat.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+    private android.support.v7.widget.Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+
+        initToolbar();
+
+
+
+
+    }
+
+    private void initToolbar() {
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setTitleTextColor(getColor(R.color.colorBackground));
+        toolbar.setOnMenuItemClickListener(new android.support.v7.widget.Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+    }
+
+
+}
