63af3437913192ff8b69632a23b6c43c1959bedc
mahsat <fahrenheitx@list.ru>
2016-05-18 13:39:20 +0300

    Implemented navigation view and tab navigation with test fragment.

--- app/src/main/java/com/example/mahsat/remindme/MainActivity.java
+++ app/src/main/java/com/example/mahsat/remindme/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.mahsat.remindme;
 
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+
+import com.example.mahsat.remindme.adapter.TabsPagerFragmentAdapter;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private android.support.v7.widget.Toolbar toolbar;
     DrawerLayout drawerlayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         initToolbar();
         initNavigationView();
+        initTabLayout();
 
 
 
@@ -35,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitleTextColor(getColor(R.color.colorBackground));
         toolbar.setOnMenuItemClickListener(new android.support.v7.widget.Toolbar.OnMenuItemClickListener() {
             @Override
-            public boolean onMenuItemClick(MenuItem item) {
+            public boolean onMenuItemClick(MenuItem menuItem) {
                 return false;
             }
         });
@@ -45,7 +53,39 @@ public class MainActivity extends AppCompatActivity {
 
     private void initNavigationView() {
         drawerlayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawerlayout, toolbar, R.string.Open, R.string.Close);
+        drawerlayout.setDrawerListener(toogle);
 
+        toogle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                drawerlayout.closeDrawers();
+                switch (menuItem.getItemId()){
+                    case R.id.actionNotificationItem:
+                        showNotificationTab();
+                }
+                return true;
+            }
+        });
+
+    }
+
+    private void initTabLayout() {
+
+        TabLayout tabLayout;
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    private void showNotificationTab(){
+        viewPager.setCurrentItem(Constants.TAB_TWO);
     }
 
 
